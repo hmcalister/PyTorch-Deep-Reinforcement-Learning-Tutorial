bbf58dba6eb5d27cffa084e119314f7c7635a23b
Hayden McAlister <haydenmcalister49@gmail.com>
2024-01-11 23:18:06 +1300

    Add hyperparameters from tutorial

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ import torch.optim as optim
 env = gym.make("CartPole-v1")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
     else:
-        print("Device is not set to cuda")
 
 X = torch.rand(10, device=device)
 checkGPUMemory()
