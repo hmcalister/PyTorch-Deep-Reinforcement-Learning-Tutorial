b88e24c0c4b69ad30e2109873756794d7ccd0b30
Hayden McAlister <haydenmcalister49@gmail.com>
2024-01-11 23:19:25 +1300

    Add method to get an action from our model given a state

--- main.py
+++ main.py
@@ -35,10 +35,26 @@ optimizer = optim.AdamW(policyNet.parameters(), lr=LR, amsgrad=True)
 memory = ReplayMemory(10000)
 steps = 0
 episodeDurations = []
-    else:
 
-X = torch.rand(10, device=device)
-checkGPUMemory()
+
+def selectAction(state):
+    global steps
+
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps / EPS_DECAY)
+    steps += 1
+    
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policyNet(state).max(1).indices.view(1, 1)
+    else:
+        # If we rolled below the epsilon threshold, do something random for exploration
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+    
+
 
 X = torch.rand(1000000, device=device)
 checkGPUMemory()
